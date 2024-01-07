0a99fc192b057725236b33ec41b7d85763161458
Marko Blažeka <markoblazeka@Markos-MacBook-Air.local>
2024-01-07 18:57:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <FormAddFriend />
+    </div>
+  );
+}
+
+export default App;
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👭 Friend Name</label>
+      <input type="text" />
+
+      <label>🌄 Image URL</label>
+      <input type="text" />
+
+      <button className="button">Add</button>
+    </form>
+  );
+}
+
+function FriendsList() {}
