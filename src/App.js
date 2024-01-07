51383f6d538e63b106358afff54affebf2067104
Marko Blažeka <markoblazeka@Markos-MacBook-Air.local>
2024-01-07 19:51:30 +0100

    third-commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,29 +22,99 @@ const initialFriends = [
 ];
 
 function App() {
+  const [data, setData] = useState(initialFriends);
+  const [addFriendForm, onAddFriendForm] = useState(false);
+
+  function handleAddFriendForm() {
+    onAddFriendForm((cur) => !cur);
+  }
+
+  const onAddFriend = (friend) => {
+    setData((cur) => [...cur, friend]);
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <FormAddFriend />
+      <div className="sidebar">
+        <FriendsList data={data} />
+        {addFriendForm ? <FormAddFriend onAddFriend={onAddFriend} /> : ""}
+        <Button onClick={handleAddFriendForm}>
+          {addFriendForm === true ? "Cancel" : "Add friend"}
+        </Button>
+      </div>
     </div>
   );
 }
 
 export default App;
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
+  const [friendName, setFriendName] = useState("");
+  const [friendImageURL, setFriendImageURL] = useState(
+    "https://i.pravatar.cc/48"
+  );
+
+  const newFriend = {
+    id: Math.random(),
+    name: friendName,
+    image: friendImageURL,
+    balance: 0,
+  };
+
+  function handleAddFriend(e) {
+    e.preventDefault();
+    onAddFriend(newFriend);
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleAddFriend}>
       <label>👭 Friend Name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={friendName}
+        onChange={(e) => setFriendName(e.target.value)}
+      />
 
       <label>🌄 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={friendImageURL}
+        onChange={(e) => setFriendImageURL(e.target.value)}
+      />
 
-      <button className="button">Add</button>
+      <Button>Add</Button>
     </form>
   );
 }
 
-function FriendsList() {
-  return <h2>Test</h2>;
+function FriendsList({ data }) {
+  return (
+    <ul>
+      {data.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p>
+        {friend.name} owes you ${friend.balance}
+      </p>
+      <button className="button">Select</button>
+    </li>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
